d3ec7701b4c984a3d3f7e87ff5c0484c614544f3
Ertugrul Yilmaz <ert.yil@gmail.com>
2020-05-15 16:47:52 +0100

    [master] add netlify config

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,31 +6,26 @@ import Navigation from '../components/Navigation';
 export default class HomePage extends React.Component {
   render() {
     return (
-      <main className="wrapper">
+      <main className='wrapper'>
         <Navigation />
 
-        <div className="content">
-          <section className="container centered">
-            <div className="about">
-              <div className="avatar">
-                <img src="/img/avatar.jpg" alt="avatar" />
+        <div className='content'>
+          <section className='container centered'>
+            <div className='about'>
+              <div className='avatar'>
+                <img src='/img/avatar.jpg' alt='avatar' />
               </div>
               <h1>Ertugrul Yilmaz</h1>
               <h2>Coder &amp; Entrepreneur &amp; Dad</h2>
               <ul>
                 <li>
-                  <a href="https://github.com/ertugrulyilmaz/">
-                    <i className="fab fa-github" />
+                  <a href='https://github.com/ertugrulyilmaz/'>
+                    <i className='fab fa-github' />
                   </a>
                 </li>
                 <li>
-                  <a href="https://twitter.com/ertugrulyilmaz/">
-                    <i className="fab fa-twitter" />
-                  </a>
-                </li>
-                <li>
-                  <a href="https://www.linkedin.com/in/yilmazertugrul/">
-                    <i className="fab fa-linkedin" />
+                  <a href='https://www.linkedin.com/in/yilmazertugrul/'>
+                    <i className='fab fa-linkedin' />
                   </a>
                 </li>
               </ul>
